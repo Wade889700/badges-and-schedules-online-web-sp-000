cb2160c1923d7b746c6db8289b8a866af1247c52
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 01:20:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,6 @@ end
 array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 def batch_badge_creator(array)
     my_array =[]
-
     array.each do |name|
     return my_array.push("Hello, my name is #{name}.")
     end
