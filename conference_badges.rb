292f0407cdb3128661775f645fd1ab91cd4dc74a
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 01:07:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 def badge_maker(name)
   return  "Hello, my name is #{name}."
 end
-
+array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 def batch_badge_creator(array)
-
+  return
 end
