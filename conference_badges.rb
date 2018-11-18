52347bcda91f2dbd9b812011faa945507d82ce0e
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-18 01:37:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-
+room = ["1","2","3","4","5","6","7"]
 def batch_badge_creator(array)
   my_array =[]
   counter = 0
@@ -15,8 +15,16 @@ def batch_badge_creator(array)
     return my_array
 end
 
-def assign_rooms(array)
-
+def assign_rooms(array,room)
+  my_array =[]
+  counter = 0
+  while counter<=array.size-1
+    name = array[counter]
+    room_number = room[counter]
+    my_array.push("Hello, #{name}! You'll be assigned to room #{room_number}, ")
+    counter += 1
+  end
+  return my_array
 end
 
 def printer
